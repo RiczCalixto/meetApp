41c7106ea25e51b4766b2a96d860682cbc41f8a2
Ricardo Calixto <rcalixtos@gmail.com>
2019-08-03 12:31:49 -0300

    validation/routes/update/craete/sessionController/userController/jwt

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'a44e7df1509681f2664b8725420dadcd',
+  expiresIn: '7d',
+};
--- src/database/migrations/20190803133803-create-users.js
+++ src/database/migrations/20190803133803-create-users.js
@@ -21,6 +21,8 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      created_at: { type: Sequelize.DATE, allowNull: false },
+      updated_at: { type: Sequelize.DATE, allowNull: false },
     });
   },
 
